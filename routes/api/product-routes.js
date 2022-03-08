6d59819d67abe6309af9e4ef37d2ba401a1d2711
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2022-03-08 14:20:34 -0600

    api routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,49 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      }
+    ]
+  })
+  .then(dbProductdata => res.json(dbProductdata))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      }
+    ]
+  })
+  .then(dbProductdata => {
+    if (!dbProductdata) {
+      res.status(404).json({ message: 'No product found wiht this id' });
+      return;
+    }
+    res.json(dbProductdata);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+// router.use((req, res) => {
+//   res.send("<h1>Still Wrong?</h1>")
+// });
+
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
+    res.status(404).end();
+  });
 
 module.exports = router;
