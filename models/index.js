3089cef84ed6756c8548e69abf4ed7b831eda0cf
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2022-03-09 18:08:12 -0600

    finish routes

--- models/index.js
+++ models/index.js
@@ -6,23 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_name'
+  foreignKey: 'product_id'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_name'
+  foreignKey: 'product_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
-// Product.belongsToMany(Tag, {
-  
-// })
+Product.belongsToMany(Tag, {
+   through: ProductTag,
+//   as: 'product_tag',
+   foreignKey: 'product_id'  
+});
 
 // Tags belongToMany Products (through ProductTag)
-// Tag.belongsToMany(Product, {
-
-// })
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+//  as: 'product_tag',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -128,6 +128,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductdata => {
+    if (!dbProductdata) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+    res.json(dbProductdata);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
